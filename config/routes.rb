979c6670a7f4543080dd6251b1495ea770ee39b2
takuma39 <takuma.sato.job@gmail.com>
2020-08-17 06:09:56 +0000

    Add routes追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admins
+#---------------------------------------------------
+    devise_for :admins, controllers: {
+        sessions:      'admins/sessions',
+        passwords:     'admins/passwords',
+    }
+#---------------------------------------------------
+
+    root 'homes#top'
+
+
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
